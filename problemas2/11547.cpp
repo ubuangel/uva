2c734e173fcd635af1e4724f16ce5ac0ede89069
angel <andresbjar97@gmail.com>
2021-09-27 16:45:33 -0500

    ejercicio

---
+++ problemas2/11547.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	cin>>numero;
+	for (unsigned int i = 0; i < numero; i += 1)
+	{
+		cin>>n;
+		n*567
+		/9
+		+7492
+		*235
+		/47
+	}
+	567*x
+}
