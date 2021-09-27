736ade481eac1820f9d4ea28e3029c3ddc845c23
angel <andresbjar97@gmail.com>
2021-09-27 17:17:47 -0500

    test ordend e operandos resultado incorrecto

--- problemas2/11547.cpp
+++ problemas2/11547.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <string>
 
-int main(){
-	int numero;
+int main(){	int  numero,n;
 	std::cin>>numero;
 	for (unsigned int i = 0; i < numero; i += 1)
 	{
-		cin>>n;
-		n=n*567
-		n=n/9
-		n=n+7492
-		n=n*235
-		n=n/47
-		n=n-498
+		std::cin>>n;
+		n=(((n*567)/9)+7492);
+//		n=n/9;
+//		std::cout<<n;
+//		n=n+7492;
+		n=n*235;
+		n=n/47;
+		n=n-498;
 		
 		//centesima digito
-		n=n/10
-		n
+		n=n/10;
+		n=n % 10;
+//		n=abs(n);
+//std::cout<<std::endl	;
+	std::cout<<n;
 	}
-	cout<<n;
 
 	
 }
