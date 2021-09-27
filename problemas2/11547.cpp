17a37ae71fc5ec12b7e06614df4bab305cbd730e
angel <andresbjar97@gmail.com>
2021-09-27 16:58:07 -0500

    ejercicio...

--- problemas2/11547.cpp
+++ problemas2/11547.cpp
@@ -2,15 +2,23 @@
 #include <string>
 
 int main(){
-	cin>>numero;
+	int numero;
+	std::cin>>numero;
 	for (unsigned int i = 0; i < numero; i += 1)
 	{
 		cin>>n;
-		n*567
-		/9
-		+7492
-		*235
-		/47
+		n=n*567
+		n=n/9
+		n=n+7492
+		n=n*235
+		n=n/47
+		n=n-498
+		
+		//centesima digito
+		n=n/10
+		n
 	}
-	567*x
+	cout<<n;
+
+	
 }
