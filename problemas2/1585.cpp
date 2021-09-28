792723c3197a588bf5323840f86b749a4596e104
angel <andresbjar97@gmail.com>
2021-09-28 08:48:06 -0500

    mm

--- problemas2/1585.cpp
+++ problemas2/1585.cpp
@@ -3,23 +3,36 @@
 using namespace std;
 
 int main(){
-	//int x=1, y=1;
+	
 	string cadena;
-		int i,cont=0;
-		int n,suma=0;
-	while( i<n ){
-		 getline(cin,cadena);
-		if(cadena[i]=='0'){
-			cont++;
-			suma=suma+cont;
-		}
-		if(cadena[i]=='X')
-		cont=0;
-		cout<<suma<<endl;
+		int i=0;
+		int n;
+		cin>>n;
+	for (unsigned int r = 0; r < n; r += 1)
+	{
+		int cont=0;
+		int suma=0;
+		getline(cin,cadena);
+		
+		while( i<cadena.size() ){
+			if(cadena[i]=='0'){
+				cont++;
+				suma=suma+cont;
+				}
+			if(cadena[i]=='X'){
+				
+				cont=0;
+			}
+		//cont=0;
 		
 		i++;
 				
-	}
+		}
+		cout<<suma<<endl;
+		
+	}	
+		
+	
 	return 0;
 	
 }
