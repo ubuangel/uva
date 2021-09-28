d4af5eb45695e45e6015fd19fca5eccdd9f19442
angel <andresbjar97@gmail.com>
2021-09-28 08:06:34 -0500

    hmm

--- problemas2/1124.cpp
+++ problemas2/1124.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
-#define resolver(x) std::cerr <<#x<<"="<<x<<std::endl;
+#define resolver(y) std::cerr <<#y<<"="<<y<<std::endl;
 int main(){
-	int x=1;
+	int x=1, y=1;
 	
-	while( x!=0){
+	while( x!=0 && y!=0){
+		std::cout<<"y=";std::cin>>y;
 		std::cout<<"x=";std::cin>>x;
 		
+				
 		resolver(x);
 	}
 	
