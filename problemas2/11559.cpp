d0616776b8072ebbe17611e028925998739b0da4
angel <andresbjar97@gmail.com>
2021-09-28 09:14:26 -0500

    11559

---
+++ problemas2/11559.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	//int x=1, y=1;
+	string cadena;
+	while( getline(cin,cadena)){
+		
+		cout << cadena << endl;
+		
+				
+	}
+	return 0;
+	
+}
