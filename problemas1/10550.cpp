4f0a174fb49a2ba6179e056c3ea299e8fb897d26
angel <andresbjar97@gmail.com>
2021-09-28 11:21:03 -0500

    validando

--- problemas1/10550.cpp
+++ problemas1/10550.cpp
@@ -3,6 +3,9 @@
 
 int main(){
 	
+int 	sumatotal=0;
+int num1,num2,num3,num4;
+cin>>num1,num2,num3,num4;
 	while(true){
 		if (num1=0&& num2=0 && num3=0 && num4=0)
 		{
@@ -13,11 +16,27 @@ int main(){
 		{
 			sumatotal+=(num2-num1);
 		}else{
-			sumatotal+=(40-num1+20);
+			sumatotal+=(40-num1+num2);
 			
 			
 		}
-		if(num1		)
+		
+		sumatotal+=40;
+		if(num3<=num2	){
+			
+			sumatotal+=(num2-num3);
+		}else{
+			sumatotal+=(40-num3+num2);
+		}
+		
+		if (num4>=num3)
+		{
+			sumatotal+=(num4-numc);
+		}else{
+			sumatotal+=(40-num3+num4);
+		}
+		
+		cout<<3220-sumatotal*9<<endl;
 		
 	}
 	
