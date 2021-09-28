4e42f83269d6f0a3de3e1a496d5a381b87e121d3
angel <andresbjar97@gmail.com>
2021-09-28 12:31:36 -0500

    ..

--- problemas1/11364.cpp
+++ problemas1/11364.cpp
@@ -3,5 +3,26 @@
 
 int main(){
 	
+	int i=0;
+	int n;
+	std::cin>>n;
+	while(i<n){
+	int d;
+	std::cin>>d;
+	int d1=0,d2=100,minimo;
+	
+		for (unsigned int i = 0; i < d; i += 1)
+			{
+			int x;
+			std::cin>>x;
+			d1=std::max(d1,x);
+			d2=std::min(d2,x);
+			}
+	
+	
+	minimo=2*(d1-d2);
+	i++;	
+	}
+	std::cout<<minimo<<std::endl;
 	
 }
