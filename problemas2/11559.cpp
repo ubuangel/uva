e27e50299c8474320dc92a15e820257dcd8122b0
angel <andresbjar97@gmail.com>
2021-09-28 10:31:59 -0500

    variables

--- problemas2/11559.cpp
+++ problemas2/11559.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+//tip :inicar escribiendo dats
 int main(){
 	//int x=1, y=1;
 	string cadena;
-	while( getline(cin,cadena)){
+	while(cin>>cadena){
 		
 		cout << cadena << endl;
+		if()
+		//--primera linea 
+		//num:numero de personas 
+		//pre:presupuesto
+		
+		//op:opciones
+		//semana:semanas a quedar
+		
+		
+		//--segundalinea 
+		
+		
+		//c :costo por persona ala semana 
+		
+		//ultima
+	//	cuartos[y ] numeros de cuartos en cada  semana y
+		//segunda linea empiza caurtos[i]=cuartos en  la semana  i  false;
+		
 		
 				
 	}
