a2264b4d6cb8ab744813aaa90cb4604cdec8dc7a
angel <andresbjar97@gmail.com>
2021-09-28 17:40:53 -0500

    12157.cpp

--- problemas2/12157.cpp
+++ problemas2/12157.cpp
@@ -11,30 +11,30 @@ int main()
 	int i=0;
 	while(i<Tarifa){
 		
-		int mileCosto=0,jugoCosto=0;
+		int mileCosto=0,jugoCosto=0,N;
 		cin>>N;
 		
 		while(N--){
-			
+			int tmp;
 			cin>>tmp;
-			mileCosto+=(1+(int )floor(tmp/30.0))*10
-						jugoCosto+=(1+(int )floor(tmp/60.0))*15
+			mileCosto+=(1+(int )floor(tmp/30.0))*10;
+						jugoCosto+=(1+(int )floor(tmp/60.0))*15;
 			
 			
 		}
 		
-		cout<<"Case"<<cont++<<":";
+		cout<<"Case "<<cont++<<": ";
 		if (mileCosto<jugoCosto)
 		{
-			cout<<"Mile"<<endl;
+			cout<<"Mile "<<mileCosto<<endl;
 		}else if(mileCosto>jugoCosto){
-			cout<<"Juice"<<endl;
+			cout<<"Juice "<<jugoCosto<<endl;
 		}else{
 			
 			cout << "Mile Juice " << mileCosto<< endl;
 		}
 		
-		i++
+		i++;
 		
 		
 	}
