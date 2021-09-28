c0975143ac0a5758e51a91d15c317e3ef6f509a0
angel <andresbjar97@gmail.com>
2021-09-28 11:14:41 -0500

    10550

---
+++ problemas1/10550.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	
+	while(true){
+		if (num1=0&& num2=0 && num3=0 && num4=0)
+		{
+			break
+		}
+		
+		if (num2>=num1)
+		{
+			sumatotal+=(num2-num1);
+		}else{
+			sumatotal+=(40-num1+20);
+			
+			
+		}
+		if(num1		)
+		
+	}
+	
+	
+}
