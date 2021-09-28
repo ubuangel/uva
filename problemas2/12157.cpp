10dac13da2beb20a2fbbcc36bca7ed807534390e
angel <andresbjar97@gmail.com>
2021-09-28 17:32:00 -0500

    12157.cpp

--- problemas2/12157.cpp
+++ problemas2/12157.cpp
@@ -8,4 +8,31 @@ int main()
 	int Tarifa, cont = 1;
 	cin >> Tarifa;
 	
+	int i=0;
+	while(i<n){
+		
+		int mileCosto=0,jugoCosto=0;
+		cin>>N;
+		
+		while(--N){
+			
+			cin>>tmp;
+			mileCosto+=(1+(int )floor(tmp/30.0))*10
+						jugoCosto+=(1+(int )floor(tmp/60.0))*15
+			
+			
+		}
+		
+		cout<<"Case"<<cont++<<":";
+		if (mileCosto<jugoCosto)
+		{
+			cout<<"Mile"<<
+		}else if(mileCosto>jugoCosto){
+			cout<<"Juice"<<
+		}
+		i++
+		
+		
+	}
+	
 }
--- problemas2/706.cpp
+++ problemas2/706.cpp
@@ -2,21 +2,16 @@
 #include <string>
 using namespace std;
 //tip :inicar escribiendo dats
-int main() {
-    while (cin >> s >> n) {
-        if (s == 0 && n == "0")
-            break;
-        // for each level of string nums[5]
-        for (int i = 0; i < 5; i++) {
-            if (i % 2 == 0) {
-                print_level(i);
-            } else {
-                for (int y = 0; y < s; y++)
-                    print_level(i);
-            }
-        }
-        
-        cout << endl;
-    }
-    return 0;
+int main(){
+	
+	string cadena;
+	while(cin>>cadena){
+		
+	
+		
+		
+				
+	}
+	return 0;
+	
 }
