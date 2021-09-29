2f91f65e5ae73ffa79d16278a40f42d01724b843
angel <andresbjar97@gmail.com>
2021-09-29 16:57:48 -0500

    1197.cpp

--- problema4/1197.cpp
+++ problema4/1197.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 int main(){
-	int it;
+	int it=0,n;
+	
+	
 	while(it<n){
+		cin>>personas>>grupos;
+		
+		if (personas==0 && grupos==0)
+		{
+			break;
+		}
+		
+		int i=0;
+		while (i<n)
+		{
+			cin>>cont;
+			
+			
+		}
 		
 		
 		
