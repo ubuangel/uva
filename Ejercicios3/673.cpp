94149875d1cf08c7a8dbd9e5b5559ec48de67416
angel <andresbjar97@gmail.com>
2021-09-28 19:16:02 -0500

    cadenas start3

--- Ejercicios3/673.cpp
+++ Ejercicios3/673.cpp
@@ -1,9 +1,27 @@
 int main(){
 	
-	int i=0;
 	
+	int C;
+	int i=0;
+	cin>>C;
 	while(i<n){
 		
+		string cadena;
+		cin>>cadena;
 		
+		for (unsigned int i = 0; i < cadena.size(); i += 1)
+		{
+			
+			if()
+			if (cadena[0]=" " && cadena[1]=" ")
+			{
+				cout<<"True";
+			}
+		}
+		
+		
+		
+		
+		i++;
 	}
 }
