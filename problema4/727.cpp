f82b501602e836e79252aafdd10cea312a149a83
angel <andresbjar97@gmail.com>
2021-09-29 18:14:01 -0500

    posfijo

--- problema4/727.cpp
+++ problema4/727.cpp
@@ -13,11 +13,44 @@ int simbolo(){
 int main(){
 	
 	int i=0;
+		char cadena1[],caracter;
+		cin>>cadena1>>cadena2;
 	while(i<n){
-		
+		satack <char> pila;
 		int caso,i;
 		
-		char cadena1,cadena2;
+		while(cin>>cadena1 && cadena1.size()){
+			
+			caracter=cadena1[0];
+			
+			if(caracter==' '){
+				break;
+				
+			}
+			if (caracter=='(')
+			{
+				pila.push(caracter);
+			}
+			
+			else if(caracter=='+'||caracter=='-'||caracter=='*'||caracter=='/'){
+				
+				while(!pila.empty()$}&& simbolo(caracter)<pila.pop()){
+					cout<<pila.top();
+					pila.pop();
+				}
+				pila.push(caracter);
+			}
+			
+			else if(c==')'){
+				
+				cout<<pila.top();
+				pila.pop();
+			}
+			
+		}
+	
+		
+		
 		
 		
 		
