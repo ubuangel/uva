0f3325e2011c8de45d1b1bd471132007b3e558ed
angel <andresbjar97@gmail.com>
2021-09-30 01:03:42 -0500

    finish

--- problema4/11136.cpp
+++ problema4/11136.cpp
@@ -12,12 +12,28 @@ int main(){
 		long costo;
 		while(i<dias){
 			
-			int k;
+			int k;	
+			cin>>k;
 			
+			for (unsigned int t = 0; t < k; t += 1)
+			{
+				int q;
+				cin>>q;
+				contenedor.insert(q);
+			}
+			
+			set<int>::iterator menor=contenedor.begin();
+			set<int>::iterator mayor=--contenedor.end();
+			
+			
+			costo=costo+*mayor-*menor;
+			
+			contenedor.erase(menor);
+			contenedor.erase(mayor);
 			
 			i++;
 		}
-		
+		cout<<costo<<'\n';
 		
 	}
 	
