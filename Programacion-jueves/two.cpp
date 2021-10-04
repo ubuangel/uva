6bec48d03648b8ef163bb732c580e7aac29bc138
angel <andresbjar97@gmail.com>
2021-10-04 14:58:49 -0500

    buscar po extremos

---
+++ Programacion-jueves/two.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+#include<vector> 
+#include <algorithm>
+using namespace std;
+
+bool busqueda(vector<int> A ,int alto,int buscar ){
+    int medio;
+    while (buscar==iterador)
+    {
+    	if (buscar<A[iterador])
+    	{
+    		iterador++;
+    	}
+    	else if(buscar>A[iterador])
+    		iterador++;
+    }
+    
+}
+
+int main()
+{
+   
+
+    
+ 
+     cout<<"ingresar numero a buscar :"<<endl;
+     int buscar;cin>>buscar;
+    
+     
+  
+    vector <int> A{4,5,7,1,3,2,8};
+    for (unsigned int i = 0; i < A.size(); i += 1)
+    {
+    	cin>>A[i];
+    }
+    
+	
+        
+        
+        
+        
+       
+     
+
+
+
+
+ 
+ return 0;
+ 
+    
+    }
+    
+    
+   
+
+
