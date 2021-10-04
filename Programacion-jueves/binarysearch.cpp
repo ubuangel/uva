2d095afff7e6a62adf319f3705baa9a040599295
angel <andresbjar97@gmail.com>
2021-10-04 11:00:55 -0500

    Violación de segmento (core generado) al ingresar por consola el tamaño y un numero

--- Programacion-jueves/binarysearch.cpp
+++ Programacion-jueves/binarysearch.cpp
@@ -10,7 +10,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include<vector> 
 using namespace std;
 
-bool busqueda(int A[] ,int alto,int buscar ){
+bool busqueda(vector<int> A ,int alto,int buscar ){
     int medio;
     int minimo=0;
     while(minimo<=alto){
