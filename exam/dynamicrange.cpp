a25dcb8dcb6d01ae770fc95f569da4d61c8b6481
angel <andresbjar97@gmail.com>
2021-10-09 18:11:14 -0500

    fix flujo de entarda else if

--- exam/dynamicrange.cpp
+++ exam/dynamicrange.cpp
@@ -6,17 +6,30 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include<vector> 
 #include <algorithm>
-using namespace std;
-void actualizar(int array[],int a,int b){
+
+
+const int maxN = 2e5;
+
+int array[maxN+1];
+
+void actualizar(int a,int b){
 	
 	array[a]=b;
 }
-int  sumarango(int array[],int a,int b){
+int  sumarango(int a,int b){
 	int sum=0;
-	for (unsigned int i = a; i <=b; i += 1)
+	for ( int i = a; i <=b; i += 1)
 	{
 		sum+=array[i];
 	}
@@ -27,23 +40,24 @@ int main(){
 	
 	int n ,q;
 	int n1,n2,n3;
-	cin>>n>>q;
-	int array[n];
-	for (unsigned int i = 1; i <=n; i += 1)
+	std::cin>>n>>q;
+	
+	for (int i = 1; i <=n; i += 1)
 	{
-		cin>>array[i];
+		std::cin>>array[i];
 	}
 	
-	for (unsigned int j = 0; j < q; j += 1)
+	for ( int j = 0; j < q; j += 1)
 	{
-		cin>>n1>>n2>>n3;
+		std::cin>>n1>>n2>>n3;
 		
-		if (n1==1)
+		if (n1==2)
 		{
-			actualizar(array,n2,n3);
-		}
+			std::cout<<sumarango(n2,n3)<<std::endl;
+		}else{ 
 		
-		cout<<sumarango(array,n2,n3)<<endl;
+			actualizar(n2,n3);}
+		
 	}
 	
 	
