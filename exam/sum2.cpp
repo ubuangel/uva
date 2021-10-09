22ae18b82eee4b3ff487697f54f8445074245cb2
angel <andresbjar97@gmail.com>
2021-10-09 13:30:13 -0500

    ...

---
+++ exam/sum2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<vector> 
+#include <algorithm>
+using namespace std;
+int main(){
+	int t,a,b;
+	cin>>t>>a>>b;
+	int array[t];
+	for (unsigned int i = 0; i < t; i += 1)
+	{
+		cin>>array[i];
+	}
+	
+	
+	
+	return 0;
+	
+}
