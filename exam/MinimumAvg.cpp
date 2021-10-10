0af4396e423e84f559fd44482104cb7dec0a580b
angel <andresbjar97@gmail.com>
2021-10-09 19:28:25 -0500

    minimum average slice...

--- exam/MinimumAvg.cpp
+++ exam/MinimumAvg.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include<vector> 
 
-
+#include<climits>
 #include <set>
 #define ln '\n'
 #define INF 1e18
 int main(){
+	int min=0;
+	int max=int_max;
 	
-	for (unsigned int i = 0; i < count; i += 1)
+	
+	vector<int>A
+	for (unsigned int i = 0; i < A.size()-2; i += 1)
 	{
-		
+		float 
 	}
 }
