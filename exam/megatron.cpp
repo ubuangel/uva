267b88ef12962c2b54ca4cfc90bf287b61bab201
angel <andresbjar97@gmail.com>
2021-10-10 00:18:44 -0500

    ...

--- exam/megatron.cpp
+++ exam/megatron.cpp
@@ -8,10 +8,11 @@ int main(){
 	int t,n,m,iter=0;
 	cin>>t;
 	vector<int>datos;
+	int j,tam=0,m_suma=0 ,m_tam=0;
 	while (iter<t)
 	{
 		cin>>n>>m;
-		tam=0;mSuma nTam;
+		tam=0;s=0;m_suma=0 ,m_tam=0;j=0;
 		
 		
 		for (unsigned int i = 0; i < n; i += 1)
@@ -25,31 +26,42 @@ int main(){
 		for (unsigned int i = 0; i < n; i += 1)
 		{
 			s+=datos[i];
-			tam´;
+			tam++;
 			
-			if (s>m && )
+			if (s<=m && tam>m_tam )
 			{
 				m_sum=s;
 				m_tam=tam;
-			}else if(tam==m_tam{
+			}else if(tam==m_tam && m_sum>s){
 				
 				m_lem=lem;
 				m_sum=s;
 			}
 			while (s>m)
 			{
-				
+				s-=dato[j];
+				j++;
 				tam--;
 			}
-			if (s<m && tam)
+			
+			
+			if (s<=m && tam>m_tam)
+			
 			{
+				m_sum=s;
+				m_tam=tam;
+			}else if(tam=m_tam && m_sum>s){
+				m_tam=tam;
+				m_sum=s;
 				
 			}
 			
 		}
+		cout<<m_sum<<" "<<m_tam<<endl;
 		
 		
 		
 		iter++;
 	}
+	return 0;
 }
