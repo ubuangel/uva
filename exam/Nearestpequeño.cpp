b16a93f13c3a1e95bac39146d8408eaa8e1451d4
angel <andresbjar97@gmail.com>
2021-10-09 21:28:14 -0500

    el  menor mas  cercano... stack complejidad n

--- exam/Nearestpequeño.cpp
+++ exam/Nearestpequeño.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include<vector> 
 #include <algorithm>
+#include<stack>
 
 
 typedef long long tipogrande; 
 
+using namespace std;
+
 int main(){
 	int n;
 	cin>>n;
-	stack<pair<int>> pila;
+	stack<pair<int,int>> pila;
 	vector<int>valores(n);
 	
 	
@@ -18,15 +21,21 @@ int main(){
 		cin>>valores[i];
 	}
 	
-	for (unsigned int i = 0; i < count; i += 1)
+	for (unsigned int i = 0; i < n; i += 1)
 	{
 		while(pila.size()>0 && pila.top().first>=valores[i]){
 			pila.pop();
 			
 			if (pila.size()>0)
 			{
-				cout<<pila.top().
+				cout<<pila.top().second<<" ";
+			}else{
+				
+				cout<<0<<" ";
+				
 			}
+			
+			pila.push({valores[i],i+1});
 			
 		}
 	}
