354a9ea7fe1ec9d494e548ee7eecd602b7c0fe9d
angel <andresbjar97@gmail.com>
2021-10-09 23:48:16 -0500

    megatron

---
+++ exam/megatron.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector> 
+#include <algorithm>
+
+int main(){
+	cin>>n;
+	int A[n];
+	
+	for (unsigned int i = 0; i < n; i += 1)
+	{
+		cin>>A[i];
+	}
+	
+	for (unsigned int i = 0; i < count; i += 1)
+	{
+		for (unsigned int i = 0; i < count; i += 1)
+		{
+			
+		}
+	}
+}
