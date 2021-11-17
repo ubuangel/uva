032ad49e0628b83a7890e90d5649893a1c9f91ca
angel <andresbjar97@gmail.com>
2021-11-17 16:34:30 -0500

    test

--- Programacion-jueves/parcial2/patternfind.cpp
+++ Programacion-jueves/parcial2/patternfind.cpp
@@ -4,7 +4,27 @@
 
 using namespace std;
 
+
+
 int main(){
+	int i;
+	cin>>i;
+	
+	string cad="nuevo de nuevo";
+	while (--i)
+	{
+		
+		int sum=0;
+		for (unsigned int i = 0; i < cad.length(); i += 1)
+		{
+			sum=sum+int(cad[i]);
+			
+			cout<<sum % 3;
+		}
+		
+		
+		
+	}
 
 	cout<<" "<<endl;
 
