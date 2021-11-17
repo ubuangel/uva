e3872f0facf4a83bd23818a2c264980596e65825
angel <andresbjar97@gmail.com>
2021-11-17 17:10:29 -0500

    adecuando la entrada de cadenas con espacios

--- Programacion-jueves/parcial2/patternfind.cpp
+++ Programacion-jueves/parcial2/patternfind.cpp
@@ -13,18 +13,25 @@ int main(){
 	while(i--){
 		
 		
-			  string str1 = "aabbabababbbaabb";
-   string str2 = "abb";
-   int pos = 0;
-   int index;
-   while((index = str1.find(str2, pos)) != string::npos) {
+		
+		
+		
+	string str1 ;
+	cin>>str1;
+	   string str2 ;
+	   cin>>str2;
+	   
+	   
+	   
+	   
+	   int pos = 0;
+	   int index;
+	   while((index = str1.find(str2, pos)) != string::npos) {
       cout << "encontrado en la posicion: " << index << endl;
       pos = index + 1; //new position is from next element of index
-   }
+  		 }
 		
 	}
 	
 	
-
-
 }
