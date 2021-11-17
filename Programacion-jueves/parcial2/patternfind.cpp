27f938e07a0e1945b58081cca9498f3b8bdd5f9d
angel <andresbjar97@gmail.com>
2021-11-17 17:06:13 -0500

    input

--- Programacion-jueves/parcial2/patternfind.cpp
+++ Programacion-jueves/parcial2/patternfind.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 
 int main(){
+	
 	int i;
 	cin>>i;
+	while(i--){
+		
+		
+			  string str1 = "aabbabababbbaabb";
+   string str2 = "abb";
+   int pos = 0;
+   int index;
+   while((index = str1.find(str2, pos)) != string::npos) {
+      cout << "encontrado en la posicion: " << index << endl;
+      pos = index + 1; //new position is from next element of index
+   }
+		
+	}
 	
-	string cad="nuevo de nuevo";
 	
-	if (cad.find("nue") != std::string::npos) {
-    std::cout << "found!" << '\n';
-}
-	cout<<" "<<endl;
+
 
 }
