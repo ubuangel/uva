d8d21b79a8cb6ee26183f3716a23256bb701f7a1
angel <andresbjar97@gmail.com>
2021-12-09 02:54:38 -0500

    validando entrada

---
+++ Programacion-jueves/unidad3/lifeforma.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector> 
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+int i;
+
+	while (cin>>i && i!=0)
+	{
+	
+	string a;
+	
+		for (unsigned int t = 0; t < i; t += 1)
+		{
+		
+		cin>>a;
+		
+		}
+		
+		
+	}
+
+}
