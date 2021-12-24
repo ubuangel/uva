60cc76f59c8a576208d10e8d539860232a9482f0
angel <andresbjar97@gmail.com>
2021-12-24 13:06:57 -0500

    conjetura de goldbach ingresar valores entrada de datos

---
+++ Programacion-jueves/unidad3/EjerciciosMiercoles/goldbach.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+
+
+
+
+int main(){
+int a;
+  while (cin>>a && a!=0) {
+    /* code */
+    for (size_t i = 0; i < count; i++) {
+      /* code */
+    }
+  }
+
+  cout<<" {}"<<endl;
+
+  return 0;
+
+
+}
