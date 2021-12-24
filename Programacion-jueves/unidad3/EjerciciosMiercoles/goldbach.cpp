3f0672c17450016e7fdffa7735e7035539f35a25
angel <andresbjar97@gmail.com>
2021-12-24 13:50:10 -0500

    si un numero es primo

--- Programacion-jueves/unidad3/EjerciciosMiercoles/goldbach.cpp
+++ Programacion-jueves/unidad3/EjerciciosMiercoles/goldbach.cpp
@@ -4,23 +4,44 @@
 #include<vector>
 
 using namespace std;
+#define grande 1000000
+
+bool primo(int a){
+
+    for (size_t i = 2; i <a; i++) {
+      /* code */
+      if (a%i==0) {
+        return false;
+      }
 
 
 
+    }
+
+  }
 
 
 int main(){
 int a;
-  while (cin>>a && a!=0) {
-    /* code */
-    for (size_t i = 0; i < count; i++) {
+    while (cin>>a && a!=0 ) {
       /* code */
+
+      if (a>2) {
+        if(primo(a)){
+
+          cout<<"primo";
+        }else{
+            cout<<"no primo";
+
+        }
+      }else{
+
+        cout<<"ingrese un numer omayor a 2"<<endl;
+      }
+
+
+
     }
-  }
-
-  cout<<" {}"<<endl;
-
-  return 0;
 
 
 }
